d4784dc38544d20bfdd265ceb3cf0dd646bc68b8
Mardon1207 <mardonsultonov8@gmail.com>
2023-05-28 21:21:51 +0500

    1

--- create_one_attribute.py
+++ create_one_attribute.py
@@ -1,2 +1,8 @@
 #Create a "Person" class
 #Create an attribute "name" in the "Person" class
+class Person:
+    name="Mardon"
+    def __init__(self,n):
+        self.name=n
+# x=Person("Mardon")
+# print(x.name)
